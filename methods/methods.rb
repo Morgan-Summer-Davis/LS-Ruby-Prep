f55e437420f9f1e0cff8533527a284d40e04c742
Morgan Davis <mdavisillustration@gmail.com>
2023-01-06 02:53:34 +0000

    Completed exercises under Methods.

---
+++ methods/methods.rb
@@ -0,0 +1,28 @@
+# Problem 1
+def greeting(name)
+  return "Hello #{name}"
+end
+
+# Problem 2
+# 2, nil, "Joe", "four", nil
+
+# Problem 3
+def multiply(a, b)
+  return a * b
+end
+
+# Problem 4
+# nil
+
+# Problem 5
+def scream(words)
+  words = words + "!!!!"
+  puts words
+end
+
+scream("Yippeee")
+# This will return nil.
+
+# Problem 5
+# The method calculate_product requires two arguments, but was called using
+# only one.
