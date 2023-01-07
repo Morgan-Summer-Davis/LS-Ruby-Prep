8f9e1daad0a224128091f8048788e911463ff5d3
Morgan Davis <mdavisillustration@gmail.com>
2023-01-07 18:25:21 +0000

    Completed exercises under Flow Control.

---
+++ flow_control/flow_control.rb
@@ -0,0 +1,41 @@
+# Problem 1
+# false
+# false
+# false
+# true
+# true
+
+# Problem 2
+def capitalize_if_over_ten(str)
+  if str.length >= 10 then str.upcase end
+end
+
+# Problem 3
+def relation_to_50(n)
+  if n < 0
+    puts 'The number is negative.'
+  elsif n > 0 && n <= 50
+    puts 'The number is between 0 and 50.'
+  elsif n > 50 && n <= 100
+    puts 'The number is between 50 and 100.'
+  else
+    puts 'The number is over 100.'
+  end
+end
+
+# Problem 4
+# FALSE
+# Did you get it right?
+# Alright now!
+
+# Problem 5
+# The method has an end method, but the if/else does not. Add an end between
+# lines 5 and 6.
+
+# Problem 6
+# error
+# false
+# false
+# true
+# false
+# true
